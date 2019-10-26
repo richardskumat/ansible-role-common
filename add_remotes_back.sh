f5cb3a671a359458498b3fbb764e0dd21282d1cc
Richard Skumat <gitlab@richardskumat.com>
2019-10-26 16:55:04 +0100

    Update local remote
    Add apparmor-profiles package to debian 10 task

--- add_remotes_back.sh
+++ add_remotes_back.sh
@@ -1,3 +1,3 @@
 git remote add github git@github:richardskumat/ansible-role-common
 git remote add gitlab git@gitlab:richardskumat/ansible-role-common
-git remote add origin git@wsgitlab:qwe/ansible-role-common
+git remote add origin git@gitea:qwe/ansible-role-common
